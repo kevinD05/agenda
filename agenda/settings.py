3b2aa2dac6e465549bb3c96bd03d14fcd84f83b4
kevinD05 <alexanderkevin05@gmail.com>
2023-06-09 14:38:08 -0600

    fix2

--- agenda/settings.py
+++ agenda/settings.py
@@ -1,5 +1,7 @@
 from pathlib import Path
 import os
+import django_heroku
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -110,9 +112,12 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
+
+django_heroku.settings(locals())
 
 
 
